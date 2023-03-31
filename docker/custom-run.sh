207bb30bc0667b98f3beccd63f65c4229b834fb8
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2023-03-31 12:03:28 +0200

    amélioration de custom-run.sh en ajoutant des commentaires

--- docker/custom-run.sh
+++ docker/custom-run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# runs 2 commands simultaneously:
-npm run server --prefix $GF_PATHS_PLUGINS/mongodb-grafana & # your second application
+# Définition de la variable d'environnement GF_PATHS_PLUGINS
+GF_PATHS_PLUGINS=/usr/share/grafana/plugins
+
+# Lancer deux applications simultanément.
+npm run server --prefix $GF_PATHS_PLUGINS/mongodb-grafana & # Seconde application
 P2=$!
-/run.sh & # your first application
+/run.sh & # Première application
 P1=$!
-wait $P1 $P2
+wait $P1 $P2 # Attendre la fin des deux applications
