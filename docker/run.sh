18b0924447ab5ed8c530b9499a439dbff06c748c
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2023-03-31 12:04:34 +0200

    amélioration de run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -5,5 +5,4 @@ pip install -r requirements &&
 nohup python twitter_producer.py & &&
 nohup python coingecko_producer.py & &&
 
-# Should use spark_submit
-nohup python spark_main_WIP.py &
+spark-submit spark_main_WIP.py
