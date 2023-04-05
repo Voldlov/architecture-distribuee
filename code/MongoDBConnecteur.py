4273f398f60805122b32aedee55e5fcd35a86c01
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2023-04-06 01:35:20 +0200

    ajout de la classe MongoDBConnecteur, sans les commentaire, à ajouter plus tard.

---
+++ code/MongoDBConnecteur.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+class MongoDBConnecteur():
+    def __init__(self, username, password, cluster, db_name) -> None:
+        self.client = MongoClient(f"mongodb+srv://{username}:{password}@{cluster}/{db_name}?retryWrites=true&w=majority")
+        self.db = self.client[db_name]
+
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+
+    def get_data(self, collection_name):
+        collection = self.get_collection(collection_name)
+        return collection.find()
+
