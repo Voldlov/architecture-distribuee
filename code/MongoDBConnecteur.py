e90f21e446a04bba988789eac97ec651707040ad
Lucas GAIO DOS SANTOS <lucas.gaildossantos@gmail.com>
2023-04-06 02:06:14 +0200

    ajout du lien dans MongDBConnecteur.

--- code/MongoDBConnecteur.py
+++ code/MongoDBConnecteur.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
 
 class MongoDBConnecteur:
-    def __init__(self, username, password, cluster, db_name) -> None:
-        self.client = MongoClient(f"mongodb+srv://{username}:{password}@{cluster}/{db_name}?retryWrites=true&w=majority")
+    def __init__(self, db_name) -> None: # username, password, cluster,
+        #self.client = MongoClient(f"mongodb+srv://{username}:{password}@{cluster}/{db_name}?retryWrites=true&w=majority")
+        self.client = MongoClient(f"mongodb+srv://Stinson:Stinson@stinson.rcfzhzz.mongodb.net/?retryWrites=true&w=majority") 
         self.db = self.client[db_name]
 
     def get_collection(self, collection_name):
